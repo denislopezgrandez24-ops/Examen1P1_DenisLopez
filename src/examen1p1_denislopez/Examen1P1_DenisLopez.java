80f52bbceaac59ffbbae85c4c78402b9314b9830
Denis J. Lopez G. <denislopezgrandez24@gmail.com>
2025-11-13 21:09:46 -0600

    menu examen

---
+++ src/examen1p1_denislopez/Examen1P1_DenisLopez.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examen1p1_denislopez;
+import java.util.Scanner;
+/**
+ *
+ * @author djlop
+ */
+public class Examen1P1_DenisLopez {
+    // static Scanner leer = new Scanner(System.in);
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner leer = new Scanner (System.in);
+        System.out.println("Menu");
+        System.out.println("1. ");
+        System.out.println("2. ");
+        System.out.println("3. ");
+        int opcion = leer.nextInt();
+    }
+    
+}
